031d61968ee85b163d1c9b6c4878e1c7b16a0b5c
Maya Messinger <maya.messinger@duke.edu>
2018-04-06 14:49:52 -0400

    Revamping, use of node and vue and components

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <persInfo>
+    </persInfo>
+  </div>
+</template>
+
+<script>
+import PersInfo from "./components/PersInfo.vue";
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components: {
+    PersInfo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  
+}
+</style>
---
+++ src/components/PersInfo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="info">
+    <div class="name">Maya Messinger</div>
+    <div class="major">BS Computer Science</div>
+    <div class="university">Duke University</div>
+    <div class="university">Class of 2019</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data () {
+    return {
+    }
+  },
+  props:	[
+  ]
+}
+</script>
+
+<style lang="scss">
+@font-face {
+  font-family: "Corbert";
+  src: url(https://mayamessinger.github.io/src/css/fonts/Corbert-Regular.otf) format("otf");
+}
+
+@font-face {
+  font-family: "Simplifica";
+  src: local("Simplifica"), url("https://mayamessinger.github.io/src/css/fonts/SIMPLIFICA Typeface.ttf") format("ttf");
+}
+
+.info {
+  margin-top: 3%;
+  text-align: center;
+}
+
+.name {
+  font-family: "Simplifica";
+  font-size: 3em;
+}
+
+.major  {
+  font-size: 2em;
+}
+
+.university {
+  font-size: 1.5em;
+}
+</style>
